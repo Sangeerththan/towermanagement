97bc0a98e1181d57f51b99433c132bc5a75cece0
sangeerththan <sangeerththan.15@cse.mrt.ac.lk>
2024-03-20 16:52:08 +0530

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import React, { useEffect } from 'react';
+// import React, { useEffect } from 'react';
 import './App.css';
 
 function App() {
-  useEffect(() => {
-    // Create a script element
-    const script = document.createElement('script');
-    script.src = 'https://lcm-solutions.appiancloud.com/suite/tempo/ui/sail-client/embeddedBootstrap.nocache.js';
-    script.id = 'appianEmbedded';
-    script.async = true;
+  // useEffect(() => {
+  //   // Create a script element
+  //   const script = document.createElement('script');
+  //   script.src = 'https://lcm-solutions.appiancloud.com/suite/tempo/ui/sail-client/embeddedBootstrap.nocache.js';
+  //   script.id = 'appianEmbedded';
+  //   script.async = true;
 
-    // Append the script to the document body
-    document.body.appendChild(script);
+  //   // Append the script to the document body
+  //   document.body.appendChild(script);
 
-    // Clean up function to remove the script when component unmounts
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, []);
+  //   // Clean up function to remove the script when component unmounts
+  //   return () => {
+  //     document.body.removeChild(script);
+  //   };
+  // }, []);
 
   return (
     <div>
